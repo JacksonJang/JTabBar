f1ae8709bcf21b6caeec1f056517a0b1ed456362
장효원 <hyo961015@naver.com>
2022-02-09 16:26:09 +0900

    Revised JTabConfig

--- Sources/JTabConfig.swift
+++ Sources/JTabConfig.swift
@@ -10,9 +10,19 @@ import UIKit
 
 public class JTabConfig {
     open var menuType:MenuType = .button
+    
+    //Menu Tab
+    open var menuTopMargin:CGFloat = 0.0
+    open var menuLeftMargin:CGFloat = 0.0
+    open var menuRightMargin:CGFloat = 0.0
+    open var menuBottomMargin:CGFloat = 0.0
     open var menuHeight:CGFloat = 50.0
+    
+    //Menu Bottom Line
     open var menuBottomLineHeight:CGFloat = 1.0
     open var menuBottomLineColor:UIColor = UIColor.black
+    
+    //Menu titles
     open var menus:[String] = []
     
     public init(menus:[String]) {
